f6de902594dc6064751763ff679f96a2e6168b1f
Christian Hinkle <christianthinkle@gmail.com>
2026-03-20 01:56:46 -0600

    [cmake][editor][files][rename] Undo changes that tried to make the ".Tools" target feel the same as the game target.

--- Code/gem_game_api_files.cmake
+++ Code/gem_game_api_files.cmake
@@ -1,8 +1,8 @@
 
 set(FILES
     Include/O3deFramework_MiscConfiguration.h
-    Include/O3deFramework_MiscBus.h
     Include/O3deFramework_MiscTypeIds.h
+    Include/O3deFramework_MiscBus.h
     Include/O3deFramework/Misc/NetConnectionEntityBus.h
     Include/O3deFramework/Misc/LevelGameEntityBus.h
     Include/O3deFramework/Misc/SettingsRegistryAccessors.h
